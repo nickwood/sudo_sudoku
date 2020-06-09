71d16a7324082c2a58135c74a09584dc2d943573
nickwood <nickgwood@gmail.com>
2020-06-09 16:03:08 +0100

    Style common elements

--- sudoku/__init__.py
+++ sudoku/__init__.py
@@ -5,16 +5,18 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     @app.route('/')
-    @app.route('/<grid>')
-    def solve(grid=None):
-        return render_template('solve.html', grid=grid)
+    @app.route('/solve')
+    # @app.route('/solve?grid=<grid>')
+    # @app.route('/<grid>')
+    def solve():
+        return render_template('solve.html')
 
     @app.route('/generate')
-    def generate(grid=None):
-        return render_template('generate.html', grid=grid)
+    def generate():
+        return render_template('generate.html')
 
     @app.route('/analyse')
-    def analyse(grid=None):
-        return render_template('analyse.html', grid=grid)
+    def analyse():
+        return render_template('analyse.html')
 
     return app

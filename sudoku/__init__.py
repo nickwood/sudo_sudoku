72fd549d9b6e48ab66d52e9f371f09511b8b5197
nickwood <nickgwood@gmail.com>
2020-06-09 11:57:45 +0100

    Initial routes

--- sudoku/__init__.py
+++ sudoku/__init__.py
@@ -1,23 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-    )
+    app = Flask(__name__)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    @app.route('/')
+    @app.route('/<grid>')
+    def solve(grid=None):
+        return render_template('solve.html', grid=grid)
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    @app.route('/generate')
+    def generate(grid=None):
+        return render_template('generate.html', grid=grid)
+
+    @app.route('/analyse')
+    def analyse(grid=None):
+        return render_template('analyse.html', grid=grid)
 
     return app

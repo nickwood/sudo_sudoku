e000ccb7476832fe8fc369e6572694b78b3e5025
nickwood <nickgwood@gmail.com>
2020-06-10 09:45:26 +0100

    Persist grid on button press

--- sudoku/__init__.py
+++ sudoku/__init__.py
@@ -1,22 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
     @app.route('/')
-    @app.route('/solve')
-    # @app.route('/solve?grid=<grid>')
-    # @app.route('/<grid>')
+    @app.route('/solve', methods=['GET', 'POST'])
     def solve():
-        return render_template('solve.html')
+        if request.method == 'GET':
+            return render_template('solve.html')
+        else:
+            post_data = request.form
+            return render_template('solve.html', data=post_data)
 
     @app.route('/generate')
     def generate():
         return render_template('generate.html')
 
-    @app.route('/analyse')
+    @app.route('/analyse', methods=['GET', 'POST'])
     def analyse():
-        return render_template('analyse.html')
+        if request.method == 'GET':
+            return render_template('analyse.html')
+        else:
+            post_data = request.form
+            return render_template('analyse.html', data=post_data)
+
 
     return app
